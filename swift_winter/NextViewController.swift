04c2528a6a2203d0d5599a9c36af1a21d51c5d67
ryutaro-kimura <sasuker2264@gmail.com>
2020-12-27 14:50:01 +0900

    First commit

---
+++ swift_winter/NextViewController.swift
@@ -0,0 +1,38 @@
+//
+//  NextViewController.swift
+//  swift_winter
+//
+//  Created by 木村龍太郎 on 2020/12/27.
+//
+
+import UIKit
+
+class NextViewController: UIViewController {
+    @IBOutlet weak var messageLabel: UILabel!
+    @IBOutlet weak var label: UILabel!
+    var passdata = 0
+        override func viewDidLoad() {
+            super.viewDidLoad()
+            label.text = String(passdata)
+
+            // Do any additional setup after loading the view.
+        }
+    @IBAction func backButton(_ sender: Any) {
+        dismiss(animated: true, completion: nil)
+        
+    }
+
+    
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
--- swift_winter/ViewController.swift
+++ swift_winter/ViewController.swift
@@ -8,12 +8,52 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    //数字を格納する場所
+    var count = 0
+    var countGoal = Int.random(in: -10...10)
+    @IBOutlet weak var countLabel: UILabel!
+    @IBOutlet weak var goalNum: UILabel!
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        goalNum.text = String(countGoal)
+    }
+    @IBAction func countUpButton(_ sender: Any) {
+            //+ボタンを押すとラベルの文字をカウントアップ
+        count = count + 1
+        //画面遷移
+        if count == countGoal {
+            performSegue(withIdentifier: "nextScreen", sender: nil)
+        }
+        countLabel.text = String(count)
+            //カウントにあわせて文字の色を変更
+            changeTextColor()
+        }
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        if segue.identifier == "nextScreen" {
+            let nextVC = segue.destination as! NextViewController
+            nextVC.passdata = count
+        }
     }
 
+    @IBAction func countDounButton(_ sender: Any) {
+         //-ボタンを押すとラベルの文字をカウントダウン
+        count = count - 1
+        countLabel.text = String(count)
+        //カウントにあわせて文字の色を変更
+        changeTextColor()
+    }
+
+//ラベルの色を変更するメソッドを定義する
+    func changeTextColor() {
+        if count >= 5 {
+            countLabel.textColor = UIColor.green
+        }else if count < 0 {
+            countLabel.textColor = UIColor.red
+        }else {
+            countLabel.textColor = UIColor.black
+        }
+    }
 
 }
 
